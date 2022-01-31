09b3e3285c1b0993754aa7044bfa8c2c136d8f0c
Vladyslav <vladyslavromanenkoo@gmail.com>
2022-01-31 18:07:38 +0200

    findAllStudents

---
+++ src/main/java/com/beerpongbeer/Demo/controller/ControllerFindAllStudents.java
@@ -0,0 +1,20 @@
+package com.beerpongbeer.Demo.controller;
+
+import com.beerpongbeer.Demo.entity.Student;
+import com.beerpongbeer.Demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ControllerFindAllStudents {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> allStudents(){
+        return studentRepository.findAll();
+    }
+}
